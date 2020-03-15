54321c2e6adeca1aaf81c03c8234b7043386d907
github username <github email address>
2020-03-15 15:09:19 -0400

    added devise

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Business.create(title: 'Pleasant Pops', address: '1781 Florida Ave NW, Washington, DC 20009', description: 'A healthy shop in Adams Morgan', website: 'https://www.pleasantpops.com/')
